8e1fed4f61c486f46a2b911ddc2ebb8a0da20478
0Tavinn <luizota32@gmail.com>
2024-10-31 15:38:02 -0300

    Projeto primeira fase

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { BrowseComponent } from './pages/browse/browse.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import path from 'node:path';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: "login",
+        component:LoginComponent
+    },
+    {
+        path:"browse",
+        component:BrowseComponent
+    },
+    {
+        path:"registro",
+        component:RegistroComponent
+    },
+    {
+        path:"**",
+        redirectTo:"login"
+    },
+];
